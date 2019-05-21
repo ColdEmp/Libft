e1dedf6d02fe0e6f2c3c0a83ff4001c0d0330162
Cameron Glanville <cglanvil@c3r5s7.wethinkcode.co.za>
2019-05-21 14:28:17 +0200

    added libft.h

---
+++ libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cglanvil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 13:15:45 by cglanvil          #+#    #+#             */
+/*   Updated: 2019/05/21 14:26:26 by cglanvil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char const *s);
+void	ft_putnbr(int n);
+int		ft_atoi(char const *s);
+
+
+#endif
