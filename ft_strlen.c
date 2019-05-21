ac135ae98ed992952b0faf3ce58f1db34fdd5e1f
Cameron Glanville <cglanvil@c3r5s7.wethinkcode.co.za>
2019-05-21 14:54:54 +0200

    added ft_strlen.c and updated libft.h

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,27 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   strlen.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cglanvil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/21 13:15:45 by cglanvil          #+#    #+#             */
-/*   Updated: 2019/05/21 14:53:41 by cglanvil         ###   ########.fr       */
+/*   Created: 2019/05/21 14:39:10 by cglanvil          #+#    #+#             */
+/*   Updated: 2019/05/21 14:43:44 by cglanvil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HEADER_H
-# define HEADER_H
+#include "libft.h"
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
+int	ft_strlen(char const *s)
+{
+	int i;
 
-void	ft_putchar(char c);
-void	ft_putstr(char const *s);
-void	ft_putnbr(int n);
-int		ft_atoi(char const *s);
-int		ft_strcmp(char const *s1, char const *s2);
-int		ft_strlen(char const *s);
-
-#endif
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
