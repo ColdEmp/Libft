34ef72e6ec76418a0527e1fd6102ebc990264c55
Cameron Glanville <cglanvil@c3r6s8.wethinkcode.co.za>
2019-06-05 13:36:14 +0200

    updated ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: cglanvil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 13:23:27 by cglanvil          #+#    #+#             */
-/*   Updated: 2019/06/03 14:35:47 by cglanvil         ###   ########.fr       */
+/*   Updated: 2019/06/05 13:33:35 by cglanvil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 	i = 0;
 	sorc = (char *)src;
 	dest = (char *)dst;
-	if (sorc == dest)
-		return (dst);
+	if (!sorc && !dest)
+		return (NULL);
 	while (i < n)
 	{
 		dest[i] = sorc[i];
