a9a16c4cf2b2b512b7d753357b029a2a02bcc0ab
Cameron Glanville <cglanvil@c3r6s8.wethinkcode.co.za>
2019-06-06 12:14:07 +0200

    added ft_strequ.c and ft_strnequ.c

--- ft_strequ.c
+++ ft_strequ.c
@@ -1,24 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnequ.c                                       :+:      :+:    :+:   */
+/*   ft_strequ.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cglanvil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/06 11:57:28 by cglanvil          #+#    #+#             */
-/*   Updated: 2019/06/06 12:12:25 by cglanvil         ###   ########.fr       */
+/*   Created: 2019/06/06 12:08:29 by cglanvil          #+#    #+#             */
+/*   Updated: 2019/06/06 12:12:01 by cglanvil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strnequ(char const *s1, char const *s2, size_t n)
+int	ft_strequ(char const *s1, char const *s2)
 {
 	int	test;
 
 	if (!s1 || !s2)
 		return (0);
-	test = ft_strncmp(s1, s2, n);
+	test = ft_strcmp(s1, s2);
 	if (test == 0)
 		return (1);
 	else
